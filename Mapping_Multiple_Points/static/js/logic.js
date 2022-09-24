ab37d700179bd746063d1c8b4e43e43e7eca4b2a
rpamintuan671 <106283411+rpamintuan671@users.noreply.github.com>
2022-09-24 10:15:13 -0700

    Mapping Single Points folder added

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -9,17 +9,15 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
-// Add circle to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",
-  fillColor: '#ffffa1'
-}).addTo(map);
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.marker(city.location).addTo(map);
+});
+  
 
-  //  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-
+// Get data from cities.js
+let cityData = cities;
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -9,6 +9,18 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
+// Add circle to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: "black",
+  fillColor: '#ffffa1'
+}).addTo(map);
+
+  //  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
