0bf95bc537d04574a50247987a6bbcbdc2ba444c
rpamintuan671 <106283411+rpamintuan671@users.noreply.github.com>
2022-09-24 12:44:44 -0700

    Final commit on Mapping Multiple Points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -9,17 +9,31 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.marker(city.location).addTo(map);
-});
-
-// Get data from cities.js
+  // Get data from cities.js
 let cityData = cities;
 
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city);
+  L.circleMarker(city.location, {
+    radius: city.population/200000,
+    color: "orange",
+    fillColor: "orange",
+    lineweight: 4
+  })
+  // commas added as separator on population (city.population.toLocaleString)
+  
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
+
+// Using MAPBLOX STYLES API
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Dark Background 
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Default background - Street view
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -11,7 +11,7 @@ let map = L.map("mapid", {
 
 // Add circle to the map for Los Angeles, California.
 L.circleMarker([34.0522, -118.2437], {
-  radius: 300,
+  radius: city.population/100000,
   color: "black",
   fillColor: '#ffffa1'
 }).addTo(map);
